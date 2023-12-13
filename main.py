8ff69f382927861e558deac92104cc2452a24964
saumya-banthia <29239098+saumya-banthia@users.noreply.github.com>
2023-12-14 02:06:34 +0530

    Added connected device status in settings

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
-import logging
 import subprocess
+import decky_plugin
+import os
 
-logging.basicConfig(filename="/tmp/template.log",
-                    format='[Template] %(asctime)s %(levelname)s %(message)s',
-                    filemode='w+',
-                    force=True)
-logger=logging.getLogger()
-logger.setLevel(logging.INFO) # can be changed to logging.DEBUG for debugging issues
+os.environ['XDG_RUNTIME_DIR'] = '/run/user/1000'
 
 
 class Plugin:
     async def up(self):
-        subprocess.run(["tailscale", "up"], timeout=10)
-        return True
+        try:
+            subprocess.run(["tailscale", "up"], timeout=10, check=False)
+            return True
+        except Exception as e:
+            decky_plugin.logger.error(e)
 
     async def down(self):
-        subprocess.run(["tailscale", "down"], timeout=10)
-        return True
+        try:
+            subprocess.run(["tailscale", "down"], timeout=10, check=False)
+            return True
+        except Exception as e:
+            decky_plugin.logger.error(e)
 
     async def get_tailscale_state(self):
-        result = not subprocess.call(["tailscale", "status"], timeout=10)
-        return result
+        try:
+            result = not subprocess.call(["tailscale", "status"], timeout=10)
+            return result
+        except Exception as e:
+            decky_plugin.logger.error(e)
+
+    async def get_tailscale_device_status(self):
+        try:
+            output = subprocess.check_output(["tailscale", "status"], timeout=10, text=True)
+            decky_plugin.logger.info(output)
+            lines = [elem for elem in output.splitlines() if len(elem) != 0]
+            output_dict = {
+                # "ip": [],
+                "name": [],
+                # "type": [],
+                "status": []
+            }
+            for line in lines:
+                parts = line.split()
+                # check if first part is an ip address
+                if not parts[0].count(".") == 3:
+                    continue
+                # output_dict["ip"].append(parts[0])
+                output_dict["name"].append(parts[1])
+                # output_dict["type"].append(parts[3])
+                output_dict["status"].append(parts[4].replace(";", ""))
+            decky_plugin.logger.info(output_dict)
+            return output_dict
+        except Exception as e:
+            decky_plugin.logger.error(e)
+
+    async def log(self, message, log_type="info"):
+        if log_type == "error":
+            decky_plugin.logger.error(message)
+            return False
+        else:
+            decky_plugin.logger.info(message)
+            return True
--- src/index.tsx
+++ src/index.tsx
@@ -8,34 +8,135 @@ import {
   SuspensefulImage,
 } from "decky-frontend-lib";
 import { VFC,
-         useState 
+         useState,
+         useEffect
         } from "react";
 import { GiMeshNetwork } from "react-icons/gi";
-import * as backend from "./backend"
 import qr from "../assets/qr.svg"
 
+const LOCAL_STORAGE_KEY_TAILSCALE_TOGGLE = 'tailscaleToggle';
 
 const Content: VFC<{ serverAPI: ServerAPI }> = ({ serverAPI }) => {
-  backend.setServer(serverAPI);
 
-  const [ tailscaleToggle, setTailscaleToggle ] = useState(false);
+  const [ tailscaleToggleState, setTailscaleToggleState ] = useState<boolean>(getInitialState(LOCAL_STORAGE_KEY_TAILSCALE_TOGGLE, false, 'switchValue'));
 
-  backend.resolvePromise(backend.getTailscaleState(), setTailscaleToggle);
+  const [ deviceStatus, setDeviceStatus ] = useState<JSX.Element>(
+  // TODO: when you have time, see if this can be replaced with ReorderableList
+  // https://wiki.deckbrew.xyz/en/api-docs/decky-frontend-lib/custom/components/ReorderableList
+  <div>
+    <span>Device Status</span>
+      <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Device 1</td>
+              <td>Online</td>
+            </tr>
+            </tbody>
+        </table>
+  </div>
+  );
+
+  function getInitialState(key: string, defaultState:any = 0, paramString:string = 'volume') {
+    const settingsString = localStorage.getItem(key);
+    if (!settingsString) {
+      return defaultState;
+    }
+    const storedSettings = JSON.parse(settingsString);
+    return storedSettings[paramString] || defaultState;
+  }
+
+  function transposeObject<T>(obj: { [key: string]: T[] }): T[][] {
+    const values = Object.values(obj);
+    const transposed: T[][] = [];
+    for (let i = 0; i < values[0].length; i++) {
+      const row: T[] = [];
+      for (let j = 0; j < values.length; j++) {
+        row.push(values[j][i]);
+      }
+      transposed.push(row);
+    }
+    return transposed;
+  }
   
   const toggleTailscale = async(switchValue: boolean) => {
-    setTailscaleToggle(switchValue);
+    setTailscaleToggleState(switchValue);
+    localStorage.setItem(LOCAL_STORAGE_KEY_TAILSCALE_TOGGLE, JSON.stringify({ switchValue }));
     await serverAPI.callPluginMethod((switchValue) ? 'up' : 'down', {});
   }
+
+  const getTailscaleState = async () => {
+    const data = await serverAPI.callPluginMethod("get_tailscale_state", {});
+    if (data.success) {
+      setTailscaleToggleState(Boolean(data.result));
+    }
+  }
+
+  const getDeviceStatus = async () => {
+    const data = await serverAPI.callPluginMethod("get_tailscale_device_status", {});
+    console.log(data);
+    if (data.success) {
+      const deviceKeys = Object.keys(data.result);
+      const transposedDeviceStatus = transposeObject(data.result);
+      console.log(transposedDeviceStatus);
+      setDeviceStatus(
+        // TODO: when you have time, see if this can be replaced with ReorderableList
+        // https://wiki.deckbrew.xyz/en/api-docs/decky-frontend-lib/custom/components/ReorderableList
+        <div>
+          <span>Device Status</span>
+          <table>
+            <thead>
+              <tr>
+                {deviceKeys.map((key) => (
+                  <th>{key}</th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {transposedDeviceStatus.map((row) => (
+                <tr>
+                  {row.map((value) => (
+                    <td>{value}</td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    }
+  }
+
+  useEffect(() => {
+    const tailscaleToggle = localStorage.getItem(LOCAL_STORAGE_KEY_TAILSCALE_TOGGLE);
+    if (tailscaleToggle) {
+      const { switchValue } = JSON.parse(tailscaleToggle);
+      setTailscaleToggleState(switchValue);
+    }
+    getTailscaleState();
+    if (tailscaleToggleState){
+      getDeviceStatus();
+    }
+  }, [serverAPI]);
 
   return (
     <PanelSection title="Settings">
         <PanelSectionRow>
           <ToggleField
           bottomSeparator='standard'
-          checked={tailscaleToggle}
+          checked={tailscaleToggleState}
           label='Toggle Tailscale'
           description='Toggles Tailscale On or Off'
           onChange={toggleTailscale} />
+        </PanelSectionRow>
+        <PanelSectionRow>
+        {/* Show Device Status */}
+          {deviceStatus}
         </PanelSectionRow>
         <PanelSectionRow>
           <div>Tailscale Setup</div>
